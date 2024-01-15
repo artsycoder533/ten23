ab68318abd9f464c09447b9b4456dc6636c823da
Natasha Johnson <njohnson533@gmail.com>
2024-01-15 16:05:07 -0500

    chore: add Hero section

---
+++ app/components/Hero.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <section className="h-[700px] flex justify-start bg-neutral rounded-md text-primary" id="home">
+      <div className="max-w-[1400px] w-[90vw] flex flex-col items-center justify-center gap-8">
+        <p className="text-7xl font-semibold w-[700px] text-center">No website builders here, just hand-coded websites.</p>
+        <p className="text-xl flex items-center gap-2 my-3">
+          Made With <FaHeart className="text-red-500" />. Get your digital footprint on the web today!  
+        </p>
+        <div className="flex gap-4">
+        <Link href="/our-work" className="bg-accent px-4 py-3 text-white self-start rounded-md">See Our Work</Link>
+        <Link href="/our-work" className="border border-accent px-4 py-3 text-accent self-start rounded-md">Learn More</Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
