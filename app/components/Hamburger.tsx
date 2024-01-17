71f5dbc296667580e9ec0df2165e387c5ad2cd71
Natasha Johnson <njohnson533@gmail.com>
2024-01-17 00:44:35 -0500

    chore: add custom colors to hamburger

--- app/components/Hamburger.tsx
+++ app/components/Hamburger.tsx
@@ -17,18 +17,18 @@ const Hamburger = ({open, setOpen}: Props) => {
       className="flex-shrink-0 w-14 h-12 flex flex-col justify-center items-center relative md:hidden border-none">
       <span
         className={
-          "w-full h-[6px] bg-[#1A8C91] rounded-lg transition-all ease-in duration-300 absolute " +
+          "w-full h-[6px] bg-primary rounded-lg transition-all ease-in duration-300 absolute " +
           (open ? "origin-center rotate-[400deg]" : "translate-y-[-16px]")
         }
       />
       <span
-        className={`w-full h-[6px] bg-black rounded-lg transition-opacity absolute ${
+        className={`w-full h-[6px] bg-highlight rounded-lg transition-opacity absolute ${
           open ? "opacity-0" : "opacity-100"
         }`}
       />
       <span
         className={
-          "w-full h-[6px] bg-[#601A7F] rounded-lg transition-all ease-in duration-300 absolute " +
+          "w-full h-[6px] bg-accent rounded-lg transition-all ease-in duration-300 absolute " +
           (open ? "origin-center -rotate-45 bg-black" : "translate-y-[16px]")
         }
       />
