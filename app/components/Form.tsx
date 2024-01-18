9d2b3240363d35e4cf1bc4920d2b4294a85dc21a
Natasha Johnson <njohnson533@gmail.com>
2024-01-17 23:02:17 -0500

    wip: netlify contact form

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useForm } from "@formspree/react";
+// import { useForm } from "@formspree/react";
 import { useState, ChangeEvent, FormEvent, useEffect } from "react";
 
 interface FormData {
@@ -14,43 +14,48 @@ function Form() {
     email: "",
     message: "",
   });
-  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM as string);
+//   const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM as string);
   const [submissionSuccess, setSubmissionSuccess] = useState<boolean>(false);
 
 
-  useEffect(() => {
-    if (state.succeeded) {
-      setFormData({
-        name: "",
-        email: "",
-        message: "",
-      });
+//   useEffect(() => {
+//     if (state.succeeded) {
+//       setFormData({
+//         name: "",
+//         email: "",
+//         message: "",
+//       });
 
-      setSubmissionSuccess(true);
+//       setSubmissionSuccess(true);
 
-      setTimeout(() => {
-        setSubmissionSuccess(false);
-      }, 5000); 
-    }
-  }, [state.succeeded]);
+//       setTimeout(() => {
+//         setSubmissionSuccess(false);
+//       }, 5000); 
+//     }
+//   }, [state.succeeded]);
 
-  const handleChange = (
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+//   const handleChange = (
+//     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+//   ) => {
+//     const { name, value } = e.target;
+//     setFormData((prevData) => ({
+//       ...prevData,
+//       [name]: value,
+//     }));
+//   };
 
   return (
     <form
+    name="ten23"
       className="space-y-8 max-w-[800px] w-[90vw] mx-auto"
-      onSubmit={handleSubmit}
+      method="POST"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    //   onSubmit={handleSubmit}
     >
       <div className="flex flex-col sm:flex-row gap-8">
         <div className="flex flex-col w-full">
+        <input type="hidden" name="form-name" value="ten23" />
           <label htmlFor="name" className="mb-2 font-medium text-white">
             Name:
           </label>
@@ -59,8 +64,8 @@ function Form() {
             name="name"
             id="name"
             className="border rounded-lg p-2 bg-neutral"
-            value={formData.name}
-            onChange={handleChange}
+            // value={formData.name}
+            // onChange={handleChange}
             required
           />
         </div>
@@ -73,8 +78,8 @@ function Form() {
             name="email"
             id="email"
             className="border rounded-lg p-2 bg-neutral"
-            value={formData.email}
-            onChange={handleChange}
+            // value={formData.email}
+            // onChange={handleChange}
             required
           />
         </div>
@@ -89,8 +94,8 @@ function Form() {
           id="message"
           rows={6}
           className="border rounded-lg p-2 bg-neutral"
-          value={formData.message}
-          onChange={handleChange}
+        //   value={formData.message}
+        //   onChange={handleChange}
           placeholder="Let us know how we can help!"
           required
         />
@@ -101,11 +106,11 @@ function Form() {
       >
         Send Message
       </button>
-      {submissionSuccess && (
-        <div className="bg-green-200 p-3 rounded-md mb-4">
-          Your message has been sent! Thank you.
+      {/* {submissionSuccess && (
+        <div className="bg-highlight p-3 rounded-md mb-4">
+          Your message has been sent! We will get back to you soon!
         </div>
-      )}
+      )} */}
     </form>
   );
 }
